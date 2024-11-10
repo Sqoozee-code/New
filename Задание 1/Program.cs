700c05d2d84f2af0bd56ddfe159853abb7a4359e
Sqoozee-code <tanirbergenarystan091@gmail.com>
2024-11-10 20:47:03 +0500

    Add files via upload

---
+++ Задание 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Answer
+{
+public static void PrintRange(int M, int N)
+{
+    if ( M > N )
+        return;
+    
+    Console.WriteLine(M);
+    PrintRange(M + 1, N);
+
+}
+static public void Main(string[] args)
+{
+int M = 1; 
+int N = 9; 
+Console.WriteLine("Числа в промежутке:");
+PrintRange(M, N);
+}
+}
+
